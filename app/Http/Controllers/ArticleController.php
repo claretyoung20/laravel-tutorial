b112fde8006bd7959c6fcd898bd3f6696bdbb1c0
Young Nnenna Maryclaret <claretyoung@gmail.com>
2020-10-03 12:28:24 +0100

    Add article table

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|Response|View
+     */
+    public function index()
+    {
+        return view('static-pages.articles', [ 'articles' => Article::latest()->get()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Article $article
+     * @return Application|Factory|Response|View
+     */
+    public function show(Article $article)
+    {
+        return view('static-pages.articles.show', ['article' => $article]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Article $article
+     * @return Response
+     */
+    public function edit(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param Article $article
+     * @return Response
+     */
+    public function update(Request $request, Article $article)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Article $article
+     * @return Response
+     */
+    public function destroy(Article $article)
+    {
+        //
+    }
+}
